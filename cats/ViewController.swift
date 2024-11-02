60e784ae96dc9d6343c680af4dfc33f87f6889ec
MrFlop0 <kaplenkovsasha03@gmail.com>
2024-11-02 19:38:13 +0300

    Initial Commit

---
+++ cats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cats
+//
+//  Created by Xenia Yarunina on 02.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ catsTests/catsTests.swift
@@ -0,0 +1,17 @@
+//
+//  catsTests.swift
+//  catsTests
+//
+//  Created by Xenia Yarunina on 02.11.2024.
+//
+
+import Testing
+@testable import cats
+
+struct catsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
